07e67265c5a19624bb25a0ff6ba5ae76f085826b
puskesmasgeyer2 <puskesmasgeyer2bangsri@gmail.com>
2026-05-08 18:12:05 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 const API_URL =
-'https://script.google.com/macros/s/AKfycbyXOBBho6NOX6QRksfs6JRfm04kbB2553fPueh-WdpseZH5YtJ5bL-Mmbgvw6LUaAlUYA/exec?action=getData';
+'https://script.google.com/macros/s/AKfycbyXOBBho6NOX6QRksfs6JRfm04kbB2553fPueh-WdpseZH5YtJ5bL-Mmbgvw6LUaAlUYA/exec?api=data';
